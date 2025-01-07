f77ded824e5ffc4a00c9f880298b0cbbb11eae6f
MightyElemental <12786983+MightyElemental@users.noreply.github.com>
2025-01-07 21:53:36 +0000

    Added temporary folders for downloading videos

--- vae/download_data.py
+++ vae/download_data.py
@@ -6,9 +6,10 @@ https://eric-xw.github.io/vatex-website/index.html
 import json
 import subprocess
 import os
-from tqdm import tqdm
+from tempfile import TemporaryDirectory
 from concurrent.futures import ThreadPoolExecutor
 from threading import Lock
+from tqdm import tqdm
 
 def split_videoID(videoID):
     last_underscore_index = videoID.rfind('_')
@@ -20,7 +21,7 @@ def split_videoID(videoID):
 
 def append_error(videoID: str, errored_file: str, lock: Lock):
     with lock:
-        with open(errored_file, "a", encoding="utf-8") as f:
+        with open(errored_file, "a+", encoding="utf-8") as f:
             f.write(videoID)
             f.write("\n")
 
@@ -47,33 +48,35 @@ def process_video(item, data_folder, errored_file, lock):
     if os.path.exists(output_dir):
         return 0
 
-    youtube_url = f'https://www.youtube.com/watch?v={youtube_id}'
-    download_command = [
-        'yt-dlp',
-        '-f', 'bestvideo[height<=720][height>=360][ext=mp4]',
-        '-N', '2',
-        '-o', f'{youtube_id}.mp4',
-        youtube_url
-    ]
-    dlcode = subprocess.run(download_command, check=False, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    with TemporaryDirectory() as tmpdir:
+        video_file = os.path.join(tmpdir, f'{youtube_id}.mp4')
+        youtube_url = f'https://www.youtube.com/watch?v={youtube_id}'
+        download_command = [
+            'yt-dlp',
+            '-f', 'bestvideo[height<=480][height>=360][ext=mp4]',
+            '-N', '2',
+            '-o', video_file,
+            youtube_url
+        ]
+        dlcode = subprocess.run(download_command, check=False, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-    if dlcode.returncode != 0:
-        append_error(videoID, errored_file, lock)
-        return 1
+        if dlcode.returncode != 0:
+            append_error(videoID, errored_file, lock)
+            return 1
 
-    os.makedirs(output_dir, exist_ok=True)
-    trim_command = [
-        'ffmpeg',
-        '-y',
-        '-ss', start_time,
-        '-to', end_time,
-        '-i', f'{youtube_id}.mp4',
-        '-r', '30',
-        os.path.join(output_dir, 'frame_%04d.jpg')
-    ]
-    trimcode = subprocess.run(trim_command, check=False, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        os.makedirs(output_dir, exist_ok=True)
+        trim_command = [
+            'ffmpeg',
+            '-y',
+            '-ss', start_time,
+            '-to', end_time,
+            '-i', video_file,
+            '-r', '30',
+            os.path.join(output_dir, 'frame_%04d.jpg')
+        ]
+        trimcode = subprocess.run(trim_command, check=False, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-    os.remove(f'{youtube_id}.mp4')
+    # os.remove(f'{youtube_id}.mp4')
 
     if trimcode.returncode != 0:
         os.removedirs(output_dir)
@@ -93,9 +96,7 @@ def download_videos_to_img_files(json_file: str, data_folder: str, num_threads: 
     errored = 0
 
     lock = Lock()  # For thread-safe file writing
-    with ThreadPoolExecutor(
-            max_workers=num_threads
-        ) as executor, \
+    with ThreadPoolExecutor(max_workers=num_threads) as executor, \
         tqdm(
             total=len(data),
             unit="video",
